6e13bc59d725954381bd3c8c1b89f3e799c1cbe7
AJ Kumar <ajanthan94@hotmail.co.uk>
2021-11-05 01:15:57 +0000

    added mongodb and saving meetups work

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,23 @@
+// /api/new-meetup
+
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://ajanthan:1nkWHS1ymVKXun6Y@cluster0.2px1e.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
